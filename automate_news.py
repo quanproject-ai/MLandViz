d92f7fe8c04f17e45ff245d3c8f4428548af8f24
wexboy <quanhan.anh@gmail.com>
2023-03-09 20:17:37 -0800

    finish automate_news app

--- automate_news.py
+++ automate_news.py
@@ -1,5 +1,5 @@
 import requests
-
+from send_emails import send_email
 api_key = "bb7a3bdcd26443b8900b696fb92e3450"
 url = "https://newsapi.org/v2/everything?q=tesla&from=2023-02-09&sortBy=publishedAt&" \
 "apiKey=bb7a3bdcd26443b8900b696fb92e3450"
@@ -11,6 +11,13 @@ request = requests.get(url)
 content = request.json()
 
 #access articles with descriptions
+body=""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+print (body)
+
+body = body.encode("utf-8")
+send_email(message=body)
+
+##app may not run because the newsapi is on trial version; hence, GET function is not working#
