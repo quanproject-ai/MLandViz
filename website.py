ff8c951aa40670a96d7e5d6e5d6bd2de7fcd008e
wexboy <quanhan.anh@gmail.com>
2023-03-09 21:45:04 -0800

    :wq :wq
    Please nter the commit message for your changes. Lines starting

--- website.py
+++ website.py
@@ -1,2 +1,15 @@
-from Flask import Flask
+from flask import Flask, render_template
+app = Flask("website")
 
+@app.route("/home/")
+def home():
+    return render_template("tutorial.html")
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+@app.route("/contact-us/")
+def contact():
+    return render_template("contact_us.html")
+
+
+app.run(debug=True)
